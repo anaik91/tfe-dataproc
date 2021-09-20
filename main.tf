4a99bcbc904dc95e4ef7b1d574a090e17c8045a1
Ashwin Naik <ashwinknaik@google.com>
2021-09-20 11:42:51 +0530

    3

--- main.tf
+++ main.tf
@@ -2,22 +2,10 @@
 Copyright 2021 Google LLC. This software is provided as-is, without warranty or representation for any use or purpose. 
 Your use of it is subject to your agreement with Google.
 */
-
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.69"
-    }
-  }
-}
-
 /******************************************
   Provider credential configuration
  *****************************************/
 provider "google" {
-  # user_project_override = false
-  # credentials           = file("my-project-d358100c9524.json")
   alias                 = "create_dataset"
   user_project_override = true
   access_token          = var.access_token
